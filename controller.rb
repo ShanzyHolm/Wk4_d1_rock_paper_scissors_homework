b429dbeebb6fdf47fa815b7ecd227136e56f2aca
Shannon Holmes <snholmes13@gmail.com>
2018-08-07 00:39:15 +0100

    update with home Welcome&Instructions erb

--- controller.rb
+++ controller.rb
@@ -5,6 +5,10 @@ require('pry')
 require_relative('models/game.rb')
 also_reload('models/*')
 
+get '/' do
+  erb(:home)
+end
+
 get '/rps/:hand1/:hand2' do
   game = Game.new(params[:hand1], params[:hand2])
   # return "#{game.rock_paper_scissors()}"
